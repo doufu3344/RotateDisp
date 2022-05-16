a06161bc9576cb8e71c494bf36f23d23ad735e01
doufu3344 <doufu3344@gmail.com>
2022-05-16 23:15:46 +0800

    feat: 界面适配高分屏

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char* argv[])
 {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication a(argc, argv);
     MainWindow w;
     //w.show();
